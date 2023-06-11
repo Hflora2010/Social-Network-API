32f1feb41a51db3ffafec8ae5bc2db86443accb8
Hailey Flora <Haileycflora@gmail.com>
2023-06-10 23:24:22 -0400

    add userController functions

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -58,7 +58,7 @@ module.exports = {
             const updatedUser = await User.findOneAndUpdate(
                 { _id: userId },
                 { $set: req.body },
-                { new: true }
+                { runValidators: true, new: true }
             );
 
           if(!updatedUser) {
@@ -70,5 +70,66 @@ module.exports = {
             console.log(err);
             return res.status(500).json(err);
         }
+    },
+
+    //DELETE user
+    async deleteUser(req, res) {
+        try  {
+            const user = await User.findOneAndRemove({ _id: req.params.userId});
+
+        if(!user) {
+            return res.status(404).json({ message: "this user does not exist"})
+        }
+        res.json({ message: "user has been deleted"});
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    //ADD friend to user friendlist
+    async addFriend (req, res) {
+        try {
+            const { userId, friendId } = req.params;
+
+            //find the user by id and update the friends array to add the friend
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: userId },
+                {$addToSet: { friends: friendId } }, //add friendId to friends array
+                {runValidators: true, new: true }
+            );
+            
+            if(!updatedUser) {
+                return res.status(404).json({ message: "user not found"});
+            }
+
+            res.json(updatedUser);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    //REMOVE friend from user friendlist
+    async removeFriend (req, res) {
+        try {
+            const { userId, friendId } = req.params;
+
+            //find the user by id and update the friends array to remove the friend
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: userId },
+                { $pull: { friends: friendId } }, //remove friendId from friends array
+                { runValidators: true, new: true }
+            );
+
+            if(!updatedUser) {
+                return res.status(404).json({ message: "user not found"});
+            }
+
+            res.json(updatedUser);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     }
-}
+};
